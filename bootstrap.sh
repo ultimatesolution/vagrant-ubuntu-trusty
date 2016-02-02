ff1a1bd6d24342b29b4abbe35956684d36135a37
John Smith <john@devhq.io>
2016-02-02 14:42:42 +0300

    add screen

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 sudo apt-get update &&\
 sudo apt-get install -y git make binutils \
 	build-essential make binutils autoconf automake \
-	autotools-dev libtool pkg-config \
+	autotools-dev libtool pkg-config screen \
 	&&\
+cp /vagrant/.screenrc /home/vagrant/ &&\
 echo bootstrap ok
 
