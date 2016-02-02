6c0681482dbfa8bf3167cd3ab0c8894dfc19d262
John Smith <john@devhq.io>
2016-02-02 14:31:03 +0300

    init commit

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+# add bootstrap commands here
+
+sudo apt-get update &&\
+sudo apt-get install -y git make binutils \
+	build-essential make binutils autoconf automake \
+	autotools-dev libtool pkg-config \
+	&&\
+echo bootstrap ok
+
